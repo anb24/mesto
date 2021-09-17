07c78348d421856511826e20c64b8c105dc0a3f3
AndreyB <AnB.124@yandex.ru>
2021-09-18 01:11:01 +0700

    исправляет ТЗ и др.
    18,09,2021 ревью 3

--- scripts/index.js
+++ scripts/index.js
@@ -5,19 +5,32 @@ const page = document.querySelector('.page');
 const leadProfileName = document.querySelector('.profile__name');
 const leadProfileDescription = document.querySelector('.profile__description');
 
-let formElement = popup.querySelector('.popup__content');
-let nameInput = formElement.querySelector('.popup__text_user-name');
-let jobInput = formElement.querySelector('.popup__text_comment');
+let formElement = popup.querySelector('.popup__form-subscribe');
+let nameInput = formElement.querySelector('.popup__text_type_name');
+let jobInput = formElement.querySelector('.popup__text_type_comment');
 
-function popupToggle() {
-  popup.classList.toggle('popup_opened');
-  page.classList.toggle('page_no-scroll');
+function openPopup() {
+  popup.classList.add('popup_opened');
+  page.classList.add('page_no-scroll');
   setPopupInputValue();
+}
+
+function closePopup() {
+  if (popupCloseBtn.classList.contains('popup__close')) {
+    popupCloseBtn.closest('.popup').classList.remove('popup_opened');
+  }
+  page.classList.remove('page_no-scroll');
+}
+
+function formSubmitHandler (evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  setNodeTextValue();
+  closePopup(); // После сохранения, закрывает popup.
 }
 
 function clickOverlay(event) {
   if (event.target === event.currentTarget) {
-    popupToggle();
+    closePopup();
   }
 }
 
@@ -29,29 +42,35 @@ function setPopupInputValue() {
 function setNodeTextValue() {
   leadProfileName.textContent = nameInput.value;
   leadProfileDescription.textContent = jobInput.value;
-
-}
-
-function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-    setNodeTextValue();
-    popupToggle(); // После сохранения, закрывает popup.
 }
 
 popup.addEventListener('click', clickOverlay);
-popupOpenBtn.addEventListener('click', popupToggle);
-popupCloseBtn.addEventListener('click', popupToggle);
+popupOpenBtn.addEventListener('click', openPopup);
+popupCloseBtn.addEventListener('click', closePopup);
 
 formElement.addEventListener('submit', formSubmitHandler);
 
 
 
-// Тут делал ЛАЙК, но он чёт только на первую кнопку (наверное не .querySelector):
+// Тут фрагменты старого кода:
 
-// let like = document.querySelector('.element__like');
-
-// function likeToggle() {
-//   like.classList.toggle('element__like_active');
+// function popupToggle() {
+//   popup.classList.toggle('popup_opened');
+//   page.classList.toggle('page_no-scroll');
+//   setPopupInputValue();
 // }
 
-// like.addEventListener('click', likeToggle);
+// function clickOverlay(event) {
+//   if (event.target === event.currentTarget) {
+//     popupToggle();
+//   }
+// }
+
+// function formSubmitHandler (evt) {
+//     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+//     setNodeTextValue();
+//     popupToggle(); // После сохранения, закрывает popup.
+// }
+
+// popupOpenBtn.addEventListener('click', popupToggle);
+// popupCloseBtn.addEventListener('click', popupToggle);
