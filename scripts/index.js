13512d6278b838926257fa6f89543303fd43763f
AndreyB <AnB.124@yandex.ru>
2021-09-18 23:39:46 +0700

    исправляет недочёты
    18,09,2021 на ревью 4

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,7 @@ const page = document.querySelector('.page');
 const leadProfileName = document.querySelector('.profile__name');
 const leadProfileDescription = document.querySelector('.profile__description');
 
-let formElement = popup.querySelector('.popup__form-subscribe');
+let formElement = popup.querySelector('.popup__form');
 let nameInput = formElement.querySelector('.popup__text_type_name');
 let jobInput = formElement.querySelector('.popup__text_type_comment');
 
@@ -16,9 +16,7 @@ function openPopup() {
 }
 
 function closePopup() {
-  if (popupCloseBtn.classList.contains('popup__close')) {
-    popupCloseBtn.closest('.popup').classList.remove('popup_opened');
-  }
+  popup.classList.remove('popup_opened');
   page.classList.remove('page_no-scroll');
 }
 
@@ -49,28 +47,3 @@ popupOpenBtn.addEventListener('click', openPopup);
 popupCloseBtn.addEventListener('click', closePopup);
 
 formElement.addEventListener('submit', formSubmitHandler);
-
-
-
-// Тут фрагменты старого кода:
-
-// function popupToggle() {
-//   popup.classList.toggle('popup_opened');
-//   page.classList.toggle('page_no-scroll');
-//   setPopupInputValue();
-// }
-
-// function clickOverlay(event) {
-//   if (event.target === event.currentTarget) {
-//     popupToggle();
-//   }
-// }
-
-// function formSubmitHandler (evt) {
-//     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-//     setNodeTextValue();
-//     popupToggle(); // После сохранения, закрывает popup.
-// }
-
-// popupOpenBtn.addEventListener('click', popupToggle);
-// popupCloseBtn.addEventListener('click', popupToggle);
